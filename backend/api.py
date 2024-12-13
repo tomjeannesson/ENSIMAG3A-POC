6794b2db7f58959f0f2abff0b7e6a53d45232e03
Adame Abdelaziz <adame.abdelaziz@gmail.com>
2024-12-13 09:22:49 +0100

    connexion between back and front

--- backend/api.py
+++ backend/api.py
@@ -1,36 +1,16 @@
 import json
-
 from fastapi import FastAPI
 from board import Board
 
 app = FastAPI()
 
-
-# List of list
-
-
-a = { "board" : [["a","a","a","a","a","a","a","a"],
-    ["a","a","a","a","a","a","a","a"],
-    ["a","a","a","a","a","a","a","a"],
-    ["a","a","a","a","a","a","a","a"],
-    ["a","a","a","a","a","a","a","a"],
-    ["a","a","a","a","a","a","a","a"],
-    ["a","a","a","a","a","a","a","a"],
-    ["a","a","a","a","a","a","a","a"]
-    ] }
-
-
-pour_Julian = []
-game_test = Board()
-for i in range(8):
-    for j in range(8):
-        pour_Julian.append (game_test.board[0][0].name)
-        
-
-
-
 @app.get("/")
 def read_root():
-    return a
-
-#Return un dictionnaire qui contient les données que je veux envoyer
+    game_test = Board()
+    boardJson = []
+    for i in range(8):
+        row = []
+        for j in range(8):
+            row.append(game_test.board[i][j].name)
+        boardJson.append(row)
+    return { "board": boardJson }
