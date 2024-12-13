b45a6483e80d9ddcfe1a64241d1de3efc426fc6b
Tom JEANNESSON <tomjeannesson@gmail.com>
2024-12-13 09:50:03 +0100

    Minor commit @ Ven 13 déc 2024 09:27:11 CET

--- backend/api.py
+++ backend/api.py
@@ -1,10 +1,28 @@
 import json
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from board import Board
 
 app = FastAPI()
+
+origins = [
+    "http://poc-thaj:8000/",
+    "https://poc-thaj:3000/",
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 board = Board()
 
 
