ed86218b3f2bf3b68e993583d9eb7be0ab8e9be4
Tom JEANNESSON <tomjeannesson@gmail.com>
2024-12-13 10:19:13 +0100

    Minor commit @ Ven 13 déc 2024 09:27:11 CET

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -72,7 +72,7 @@ export default function Home() {
     const  fetchData =  async ()=>{
       setInterval(async ()=>{ 
         const res = await axios.get("http://poc-thaj:8000/")
-        console.log(res)
+        setBoard(res.data)
     }, 1000);
   }
   fetchData()
