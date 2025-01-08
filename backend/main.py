8f5bb892ff26ba05eec826770ea9f5dc89f04713
Tom JEANNESSON <tomjeannesson@gmail.com>
2025-01-08 17:04:51 +0100

    Minor commit @ Mer  8 jan 2025 15:10:13 CET

--- backend/main.py
+++ backend/main.py
@@ -21,6 +21,6 @@ while True:
         with open(data_file_path, "w") as f:
             print(data.decode())
             json.dump(
-                f"000000000000001111111111111111111111111111111{data.decode()[1]}{data.decode()[0]}100000{data.decode()[3]}{data.decode()[2]}000000000",
+                f"000000000000001111111111111111111111111111111{data.decode()[0]}{data.decode()[1]}100000{data.decode()[2]}{data.decode()[3]}000000000",
                 f,
             )
